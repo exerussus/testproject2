23710d167c757dbd137ea53f7e61afaa4b4eaae3
Taranis <solovyov.production@gmail.com>
2023-06-22 13:22:05 +0300

    Added car and some materials

---
+++ Assets/Source/Scripts/CoinColorChanger.cs
@@ -0,0 +1,28 @@
+
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinColorChanger : MonoBehaviour
+{
+    [SerializeField] private MeshRenderer _meshRenderer;
+    [SerializeField] private List<Material> _materialList;
+    private int _maxIndexCount;
+    private int _actuallyIndex = 0;
+
+    private void Start()
+    {
+        _maxIndexCount = _materialList.Count - 1;
+        SetColor(0);
+    }
+
+    private void SetColor(int index)
+    {
+        _meshRenderer.material = _materialList[index];
+    }
+    
+    public void ChangeColor()
+    {
+        _actuallyIndex = _actuallyIndex + 1 > _maxIndexCount ? 0 : _actuallyIndex + 1;
+        SetColor(_actuallyIndex);
+    }
+}
