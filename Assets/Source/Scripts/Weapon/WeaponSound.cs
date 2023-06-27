8f7b38f287c1f655409e8f9710deb7be335abd40
Taranis <solovyov.production@gmail.com>
2023-06-28 02:30:43 +0300

    Added shooting sound

---
+++ Assets/Source/Scripts/Weapon/WeaponSound.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using UnityEngine;
+
+namespace Source.Scripts.Weapon
+{
+    public class WeaponSound : MonoBehaviour
+    {
+        [SerializeField] private AudioSource _audioSource;
+        [SerializeField] private AudioClip _audioClip;
+        [SerializeField] private WeaponKeyListener _weaponKeyListener;
+
+        private void OnEnable()
+        {
+            _weaponKeyListener.OnShoot += MakeSound;
+        }
+
+        private void OnDisable()
+        {
+            _weaponKeyListener.OnShoot -= MakeSound;
+        }
+
+        private void MakeSound()
+        {
+            _audioSource.PlayOneShot(_audioClip);
+        }
+    }
+}
