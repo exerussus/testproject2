72ed210409509e5659552a9d28364f625f4a72d2
Taranis <solovyov.production@gmail.com>
2023-06-28 15:06:47 +0300

    Added enemy spawner

---
+++ Assets/Source/Scripts/Enemy/EnemySpawner.cs
@@ -0,0 +1,66 @@
+﻿
+using Source.Scripts.Movements;
+using UnityEngine;
+
+namespace Source.Scripts.Enemy
+{
+    public class EnemySpawner : MonoBehaviour
+    {
+        [SerializeField] private GameObject _parentObject;
+        [SerializeField] private GameObject[] _enemyPrefabs;
+        [SerializeField] private Transform[] _spawnersTransforms;
+        [SerializeField] private Transform _playerTransform;
+        [SerializeField] private int _maxEnemyCount = 10;
+        [SerializeField] private float _spawnTimer = 3f;
+        private float _timeCount = 0f;
+
+        private void Update()
+        {
+            if (_timeCount >= _spawnTimer)
+            {
+                _timeCount = 0f;
+                if (IsFewEnemy()) CreateEnemy();
+            }
+            else
+            {
+                _timeCount += Time.fixedDeltaTime;
+            }
+        }
+
+        private void CreateEnemy()
+        {
+            var enemyPrefab = GetRandomEnemy();
+            var spawnerTransform = GetRandomSpawnersTransform();
+            var enemy = Instantiate(enemyPrefab, spawnerTransform.position, Quaternion.identity,
+                _parentObject.transform);
+            var direction = _playerTransform.position - spawnerTransform.position;
+            var rotation = Quaternion.LookRotation(direction);
+            enemy.transform.rotation = rotation;
+            var zombieMovements =  enemy.GetComponent<ZombieMovements>();
+            zombieMovements.SetMovementTarget(_playerTransform);
+        }
+        
+        private bool IsFewEnemy()
+        {
+            return _parentObject.transform.childCount < _maxEnemyCount;
+        }
+        
+        private int GetRandomIndex(int listLength)
+        {
+            return  Random.Range(0, listLength);
+        }
+        
+        private Transform GetRandomSpawnersTransform()
+        {
+            var randomIndex = GetRandomIndex(_spawnersTransforms.Length);
+            return _spawnersTransforms[randomIndex];
+        }
+        
+        private GameObject GetRandomEnemy()
+        {
+            var randomIndex = GetRandomIndex(_enemyPrefabs.Length);
+            return _enemyPrefabs[randomIndex];
+        }
+
+    }
+}
--- Assets/Source/Scripts/Movements/ZombieMovements.cs
+++ Assets/Source/Scripts/Movements/ZombieMovements.cs
@@ -6,11 +6,23 @@ namespace Source.Scripts.Movements
     public class ZombieMovements : MonoBehaviour
     {
         [SerializeField] private float _rotationSpeed = 20f;
-        [SerializeField] private Camera _camera;
+        [SerializeField] private Transform _targetTransform;
+        private bool _isTargetTransformNull;
+
+        private void Start()
+        {
+            _isTargetTransformNull = _targetTransform == null;
+        }
+
+        public void SetMovementTarget(Transform targetTransform)
+        {
+            _targetTransform = targetTransform;
+        }
         
         private void Update()
         {
-            var direction = _camera.transform.position - transform.position;
+            if (_isTargetTransformNull) return;
+            var direction = _targetTransform.position - transform.position;
             direction.y = 0;
             direction.Normalize();
 
