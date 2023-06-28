db20670f4fcc50a9cc0ce79c7ef48cce21eb09b3
Taranis <solovyov.production@gmail.com>
2023-06-28 11:10:35 +0300

    Added zombie walking

---
+++ Assets/Source/Scripts/Movements/ZombieMovements.cs
@@ -0,0 +1,22 @@
+﻿
+using UnityEngine;
+
+namespace Source.Scripts.Movements
+{
+    public class ZombieMovements : MonoBehaviour
+    {
+        [SerializeField] private float _rotationSpeed = 20f;
+        [SerializeField] private Camera _camera;
+        
+        private void Update()
+        {
+            var direction = _camera.transform.position - transform.position;
+            direction.y = 0;
+            direction.Normalize();
+
+            var toRotation = Quaternion.LookRotation(direction, Vector3.up);
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, toRotation, 
+                _rotationSpeed * Time.deltaTime);
+        }
+    }
+}
